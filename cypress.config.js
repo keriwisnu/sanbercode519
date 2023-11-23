61dee7bd19ba0a86242eab14569157153827e978
keriwisnu <keriwisnu@gmail.com>
2023-11-24 02:31:44 +0700

    cypress config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,16 @@
-// const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress");
 
-// module.exports = defineConfig({
-//   e2e: {
-//     baseUrl: 'https://magento.softwaretestingboard.com/',
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     },
-//     defaultCommandTimeout: 5500,
-//     screenshotOnRunFailure: false,
-//     chromeWebSecurity: false
-//   },
-// });
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: 'https://magento.softwaretestingboard.com/',
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    defaultCommandTimeout: 5500,
+    screenshotOnRunFailure: false,
+    chromeWebSecurity: false
+  },
+});
 
 e2e: {
   setupNodeEvents(on, config) {
