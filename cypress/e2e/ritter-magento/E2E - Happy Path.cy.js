47557053652178c30fa2c1a248dad7dd17fda1bb
sidantez <ritter.sidante@gmail.com>
2023-11-25 01:58:45 +0800

    added Checkout and Order Succeed into E2E-HP

--- cypress/e2e/ritter-magento/E2E - Happy Path.cy.js
+++ cypress/e2e/ritter-magento/E2E - Happy Path.cy.js
@@ -39,11 +39,10 @@ function confirmPassword() {
   return conPass
 }
 
-
-
 describe("Registration", () => {
   it("Create an account", () => {
     cy.clearCookies()
+    cy.clearLocalStorage()
     cy.visit("")
     cy.wait(500)
     cy.get(".logo > img").should("be.visible")
@@ -84,29 +83,29 @@ describe("Select Product", () => {
     cy.get('img').click()
     cy.get(':nth-child(2) > .product-item-info > .product-item-details > .product-item-name > .product-item-link').click()
     cy.get('#option-label-size-143-item-166').click()
-    cy.wait(3000)
+    cy.wait(5000)
     cy.get('#option-label-color-93-item-57').click()
-    cy.wait(3000)
+    cy.wait(5000)
     cy.get('#product-addtocart-button > span').click()
-    cy.wait(3000)
+    cy.wait(5000)
     cy.get('.message-success > div').should('contain.text', 'You added Breathe-Easy Tank to your shopping cart.')
-    cy.wait(3000)
+    cy.wait(5000)
     cy.get('.counter-number').should('not.be.null')
   })
 
   it("Select product #2 - from the Header Menu", () => {
     cy.get('#ui-id-5 > :nth-child(2)').click()
     cy.get('dd > .items > :nth-child(1) > a').click()
-    cy.wait(3000)
+    cy.wait(5000)
     cy.get(':nth-child(1) > .product-item-info > .details > .name > .product-item-link').click()
-    cy.wait(3000)
+    cy.wait(5000)
     cy.get('#option-label-size-143-item-170').click()
-    cy.wait(3000)
+    cy.wait(5000)
     cy.get('#option-label-color-93-item-50').click()
-    cy.wait(3000)
+    cy.wait(5000)
     cy.get('#product-addtocart-button > span').click()
     cy.get('.message-success > div').should('contain.text', 'You added Cassius Sparring Tank to your shopping cart.')
-    cy.wait(3000)
+    cy.wait(5000)
     cy.get('.counter-number').should('not.be.null')
   })
 })
@@ -114,16 +113,16 @@ describe("Select Product", () => {
 describe("Cart", () => {
   let initialCount
   it("Open cart and remove item", () => {
-    cy.wait(500)
+    cy.wait(3000)
     cy.get('.counter-number').then((counter) => {
       initialCount = parseInt(counter.text())
       expect(initialCount).to.be.greaterThan(0)
     })
     cy.get('.showcart').click()
     cy.get(':nth-child(7) > .secondary > .action > span').click()
-    cy.wait(3000)
+    cy.wait(5000)
     cy.get(':nth-child(4) > .item-actions > td > .actions-toolbar > .action-delete').click()
-    cy.wait(3000)
+    cy.wait(8000)
     cy.get('.counter-number').then((counter) => {
       const updatedCount = parseInt(counter.text())
       expect(updatedCount).to.be.lessThan(initialCount)
@@ -131,7 +130,7 @@ describe("Cart", () => {
   })
 
   it("Add quantity of an item within the Cart", () => {
-    cy.wait(3000)
+    cy.wait(5000)
     cy.get('.counter-number').then((counter) => {
       initialCount = parseInt(counter.text())
       expect(initialCount).to.be.greaterThan(0)
@@ -148,13 +147,44 @@ describe("Cart", () => {
 
 describe("Checkout", () => {
   it("Proceed to checkout", () => {
-    cy.wait(3000)
+    cy.wait(5000)
     cy.get('.checkout-methods-items > :nth-child(1) > .action > span').click()
-    cy.wait(3000)
+    cy.wait(5000)
     cy.url().should("include", "/checkout/")
-    cy.wait(3000)
+    cy.wait(5000)
   })
 
+  it("Shipping - US with Flatrate", () => {
+    cy.wait(5000)
+    cy.url().should("include", "/checkout/#shipping")
+    cy.fixture('shipping.json').then((shipping) => {
+      cy.get('input[name="street[0]"]').type(shipping.address)
+      cy.get('input[name="city"]').type(shipping.city)
+      cy.get('select[name="region_id"]').select('Alaska')
+      cy.get('input[name="postcode"]').type(shipping.zip)
+      cy.get('select[name="country_id"]').select('United States')
+      cy.get('input[name="telephone"]').type(shipping.phone)
+      cy.get('input[type="radio"][value="flatrate_flatrate"]').check()
+      cy.get('button[data-role="opc-continue"]').click()
+    })
+  })
+
+  it("Review and Payment", () => {
+    cy.wait(5000)
+    cy.url().should("include", "/checkout/#payment")
+    cy.get('.payment-group > .step-title').should('exist').and('contain', 'Payment Method')
+    cy.get('span.title').should('exist').and('have.text', 'Order Summary')
+    cy.get('.sub > .mark').should('exist').and('have.text', 'Cart Subtotal')
+    cy.get('button.action.primary.checkout[data-bind*="Place Order"]').click()
+  })
 })
 
-
+describe("Order Succeed", () => {
+  it("Success Page", () => {
+    cy.wait(5000)
+    cy.url().should("include", "success")
+    cy.get(".base").should("contain.text", "Thank you for your purchase!")
+    cy.get('.order-number').should('exist')
+    cy.get('.checkout-success > .actions-toolbar > div.primary > .action > span').should('exist').and('have.text', 'Continue Shopping')
+  })
+})
