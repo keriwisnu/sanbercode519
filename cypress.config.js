3c0133b162d7ba06c189297aba1d334e197f4d08
sella <sella@thegreatsoft.com>
2023-11-28 16:53:31 +0700

    update cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,35 +1,15 @@
-<<<<<<< HEAD
-=======
-// module.exports = {
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     },
-//   },
-// };
-
->>>>>>> 29f5342c5d2a639d36ffb6743e25a4545debd972
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://magento.softwaretestingboard.com/',
-<<<<<<< HEAD
     env: {
       //untuk membuat variabel
     },
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    defaultCommandTimeout: 3500,
+    
   },
 });
-=======
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    defaultCommandTimeout: 5500,
-    screenshotOnRunFailure: false,
-    chromeWebSecurity: false
-  },
-});
->>>>>>> 29f5342c5d2a639d36ffb6743e25a4545debd972
